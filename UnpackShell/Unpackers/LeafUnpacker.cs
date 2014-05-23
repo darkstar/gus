1429e57ce60056714e29253493afffbd0911eda9
Michael D <michael@drueing.de>
2014-05-23 21:29:14 +0200

    Add unpackers
    - Fairy Bloom Fresia .tgp (unpacking fully supported)
    - Tears to Tiara .pak (unpacking supported but resulting files are still compressed; WiP only)

---
+++ UnpackShell/Unpackers/LeafUnpacker.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using UnpackShell.Interfaces;
+using System.ComponentModel.Composition;
+
+namespace UnpackShell.Unpackers
+{
+    [Export(typeof(IUnpacker))]
+    public class LeafUnpacker : IUnpacker
+    {
+        public string GetName()
+        {
+            return "leaf.pak";
+        }
+
+        public string GetDescription()
+        {
+            return "Leaf PAK file";
+        }
+
+        public string GetVersion()
+        {
+            return "1.0";
+        }
+
+        public UnpackerFlags GetFlags()
+        {
+            return UnpackerFlags.Experimental;
+        }
+
+        List<FileEntry> ReadDirectory(Stream strm, Callbacks callbacks)
+        {
+            List<FileEntry> results = new List<FileEntry>();
+            BinaryReader rd = new BinaryReader(strm);
+            Int32 numFiles;
+
+            if (rd.ReadUInt32() != 0x5041434b)
+                return null;
+
+            numFiles = rd.ReadInt32();
+            if (numFiles > 100000)
+                return null;
+
+            for (int i = 0; i < numFiles; i++)
+            {
+                FileEntry ent = new FileEntry();
+                byte[] namebuf = new byte[24];
+
+                ent.LongData["type"] = rd.ReadUInt32(); // 0x0, 0x1 = file, 0xcccccccc = dir?
+                if ((ent.LongData["type"] != 1) && (ent.LongData["type"] != 0) && (ent.LongData["type"] != 0xcccccccc))
+                    return null;
+
+                rd.Read(namebuf, 0, 24);
+                ent.Filename = Encoding.GetEncoding("SJIS").GetString(namebuf).TrimEnd('\0');
+                ent.Offset = rd.ReadUInt32();
+                ent.UncompressedSize = rd.ReadUInt32();
+                if (ent.UncompressedSize > 1024 * 1024 * 1024)
+                    return null;
+                if (ent.Offset > 1024 * 1024 * 1024)
+                    return null;
+
+                results.Add(ent);
+            }
+
+            return results;
+        }
+
+        public bool IsSupported(Stream strm, Callbacks callbacks)
+        {
+            return ReadDirectory(strm, callbacks) != null;
+        }
+
+        public IEnumerable<FileEntry> ListFiles(Stream strm, Callbacks callbacks)
+        {
+            return ReadDirectory(strm, callbacks);
+        }
+
+        public void UnpackFiles(Stream strm, Callbacks callbacks)
+        {
+            byte[] buffer;
+
+            foreach (FileEntry ent in ReadDirectory(strm, callbacks))
+            {
+                strm.Seek(ent.Offset, SeekOrigin.Begin);
+                if (ent.LongData["type"] == 0xcccccccc)
+                {
+                    // skip for now
+                }
+                else 
+                {
+                    buffer = new byte[ent.UncompressedSize];
+                    strm.Read(buffer, 0, (int)ent.UncompressedSize);
+                    // TODO: This is not the whole story. Some files are compressed by some form of RLE
+                    // Need to figure this out
+                    callbacks.WriteData(ent.Filename, buffer);
+                }
+            }
+        }
+
+        public void PackFiles(Stream strm, List<PackFileEntry> filesToPack, Callbacks callbacks)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnpackShell/Unpackers/TGPUnpacker.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using UnpackShell.Interfaces;
+using System.ComponentModel.Composition;
+
+namespace UnpackShell.Unpackers
+{
+    [Export(typeof(IUnpacker))]
+    public class TGPUnpacker : IUnpacker
+    {
+        public string GetName()
+        {
+            return "freesia.tgp";
+        }
+
+        public string GetDescription()
+        {
+            return "Fairy Bloom Freesia TGP file";
+        }
+
+        public string GetVersion()
+        {
+            return "1.0";
+        }
+
+        public UnpackerFlags GetFlags()
+        {
+            return UnpackerFlags.Experimental | UnpackerFlags.SupportsSubdirectories;
+        }
+
+        public bool IsSupported(Stream strm, Callbacks callbacks)
+        {
+            byte[] buf = new byte[4];
+            BinaryReader rd = new BinaryReader(strm);
+            int numFiles;
+
+            rd.Read(buf, 0, 4);  // magic check
+            if (Encoding.ASCII.GetString(buf, 0, 4) != "TGP0")
+                return false;
+
+            if (rd.ReadInt32() != 1)  // version check
+                return false;
+
+            if (rd.ReadInt32() != 0)  // should be zero
+                return false;
+
+            numFiles = rd.ReadInt32();
+            if ((numFiles <= 0) || (numFiles > 100000))  // some sanity checks
+                return false;
+
+            return true;
+        }
+
+        public IEnumerable<FileEntry> ListFiles(Stream strm, Callbacks callbacks)
+        {
+            byte[] buf = new byte[4];
+            BinaryReader rd = new BinaryReader(strm);
+            int numFiles;
+            List<FileEntry> results = new List<FileEntry>();
+
+            rd.Read(buf, 0, 4);
+            if (Encoding.ASCII.GetString(buf, 0, 4) != "TGP0")
+                return results;
+
+            if (rd.ReadInt32() != 1)  // version check
+                return results;
+
+            if (rd.ReadInt32() != 0)  // should be zero
+                return results;
+
+            numFiles = rd.ReadInt32();
+            buf = new byte[0x60];
+            for (int i = 0; i < numFiles; i++)
+            {
+                FileEntry ent = new FileEntry();
+
+                rd.Read(buf, 0, 0x60);
+                ent.Filename = Encoding.ASCII.GetString(buf);
+                ent.Filename = ent.Filename.Substring(0, ent.Filename.IndexOf('\0'));
+                ent.Offset = rd.ReadInt64();
+                ent.UncompressedSize = rd.ReadInt64();
+
+                results.Add(ent);
+            }
+
+            return results;
+        }
+
+        public void UnpackFiles(Stream strm, Callbacks callbacks)
+        {
+            foreach (FileEntry ent in ListFiles(strm, callbacks))
+            {
+                strm.Seek(ent.Offset, SeekOrigin.Begin);
+                byte[] buf = new byte[ent.UncompressedSize];
+                strm.Read(buf, 0, buf.Length);
+                callbacks.WriteData(ent.Filename, buf);
+            }
+        }
+
+        public void PackFiles(Stream strm, List<PackFileEntry> filesToPack, Callbacks callbacks)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
